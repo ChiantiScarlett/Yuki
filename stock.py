b0775b9bc52c40511a8ca4079b30e1661fadd87f
ShiinaChianti <shiina.chianti@gmail.com>
2018-01-09 11:43:55 +0900

    created Stock module, get_stock function

---
+++ stock.py
@@ -0,0 +1,124 @@
+from urllib.request import urlopen, URLError
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+class Stock:
+    """
+    name : name of the corporation
+    code : stock code of the corporation
+    timestamp : Parsed date/time
+    price : current price
+    open : open price
+    high : highest price
+    low : lowest price
+    volume : trade volume
+    market : market area (KOSPI or KOSDAQ)
+    rank : rank of the stock in the market
+    PER : PER (WISEfn)
+    foreign_rate : Foreign Consumption Rate
+    """
+
+    def __init__(self):
+        self.name = None
+        self.code = None
+        self.price = None
+        self.timestamp = None
+        self.open = None
+        self.high = None
+        self.low = None
+        self.volume = None
+        self.market = None
+        self.rank = None
+        self.PER = None
+        self.foreign_rate = None
+        self.history = []
+
+    def __str__(self):
+
+        return_str = "[{name}] ({market}, {code})\n" +\
+                     "<{timestamp}>\n" +\
+                     "Price:        {price:,}\n" +\
+                     "High:         {high:,}\n" +\
+                     "Low:          {low:,}\n" +\
+                     "Open:         {open:,}\n" +\
+                     "Trade Volume: {volume:,}"
+
+        return return_str.format(name=self.name, market=self.market,
+                                 timestamp=self.timestamp, price=self.price,
+                                 high=self.high, low=self.low, code=self.code,
+                                 open=self.open, volume=self.volume)
+
+
+def get_stock(stock_code, history=None):
+    url = "http://finance.naver.com/item/sise.nhn?code={}".format(stock_code)
+    html = BeautifulSoup(urlopen(url).read().decode('euc-kr'), 'html.parser')
+
+    stock = Stock()
+    # parse stock code
+    src = html.find('span', {'class': 'code'})
+    stock.code = src.text
+    # parse market and rank
+    src = html.find('div', {'class': 'first'}).find_all('tr')[1]
+    src = src.find('td').text
+    m_name = {'코스피': 'KOSPI', '코스닥': 'KOSDAQ'}
+    stock.market = m_name[src.split()[0]]
+    stock.rank = int(src.split()[1][:-1])
+    # parse timestamp
+    src = html.find('em', {'class': 'date'}).text.strip().split()
+    stock.timestamp = " ".join(src[:2])
+    # parse open, high, volume, low, price, name
+    src = html.find('table').find_all('span', {'class': 'blind'})
+    stock.open = int(src[4].text.strip().replace(',', ''))
+    stock.high = int(src[1].text.strip().replace(',', ''))
+    stock.volume = int(src[3].text.strip().replace(',', ''))
+    stock.low = int(src[5].text.strip().replace(',', ''))
+    src = html.find('div', {'class': 'rate_info'})
+    stock.name = src.find('dt').text.strip()
+    src = src.find('span', {'class': 'blind'}).text
+    stock.price = int(src.strip().replace(',', ''))
+    # parse PER (WISEfn)
+    src = html.find('table', {'class': 'per_table'})
+    stock.PER = float(src.find_all('tbody')[0].find('td').find('em').text)
+    # parse Foreign Consumption Rate
+    src = html.find('table', {'class': 'lwidth'}).find_all('td')[2].text
+    stock.foreign_rate = src
+
+    # if History is set, parse historical data of the item
+    if history is None:
+        return stock
+    # convert start_date, end_date into comparable string
+    START_DATE, END_DATE = map(
+        lambda x: datetime.strptime(str(x), '%Y%m%d').strftime('%Y.%m.%d'),
+        history)
+    # start_parsing
+    url = "http://finance.naver.com/item/sise_day.nhn?code={code}&page={page}"
+    for page in range(1, 100000):
+        try:
+            html = urlopen(url.format(code=stock_code, page=page))
+            html = BeautifulSoup(html.read().decode('euc-kr'), 'html.parser')
+        # if data page is out of range, stop and return stock class
+        except URLError:
+            return stock
+        # parse each row
+        for row in html.find_all('tr')[3:]:
+            src = row.find_all('span')
+            # if invalid span(including horizon border, etc), skip.
+            if len(src) == 0:
+                continue
+            # if date is out of range, skip.
+            if src[0].text.strip() > END_DATE:
+                continue
+            # if date is out of range, stop and return stock
+            if src[0].text.strip() < START_DATE:
+                return stock
+            # else parse data
+            data = {}
+            data['date'] = src[0].text.strip()
+            data['end'] = int(src[1].text.strip().replace(',', ''))
+            data['start'] = int(src[3].text.strip().replace(',', ''))
+            data['high'] = int(src[4].text.strip().replace(',', ''))
+            data['low'] = int(src[5].text.strip().replace(',', ''))
+            data['volume'] = int(src[6].text.strip().replace(',', ''))
+            # append to the 'stock' class
+            stock.history.append(data)
