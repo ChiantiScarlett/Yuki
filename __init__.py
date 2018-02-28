a8434ec858b373442f927d0aef12474bc64e81f7
Chianti Shiina <chianti@shiina.chianti>
2018-03-01 06:57:55 +0900

    modified project name from suzaku to yuki

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+# Project Yuki
+# Designed to run on iPython3 environment
+# from yuki import *
+
 import logging
 import pandas as pd
 
@@ -8,3 +12,37 @@ logging.basicConfig(level=logging.DEBUG,
 
 # Prevent DataFrame from splitting into multiple lines
 pd.set_option('display.expand_frame_repr', False)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def init_yuki():
+    from importlib import import_module
+    # Necessary pre-installed modules are as follows:
+    modules = ['setuptools',
+               'matplotlib',
+               'scipy',
+               'numpy',
+               'tabulate']
+    operatable = True
+    missing_modules = []
+    for module in modules:
+        try:
+            import_module(module)
+        except ImportError:
+            operatable = False
+            missing_modules.append(module)
+
+    if not operatable:
+        missing_modules = ", ".join(missing_modules)
+        logging.critical("Please install following module(s): {}"
+                         .format(missing_modules))
+    if operatable:
+        from yuki.parse import Stock
+        from yuki.tools import past, today, dprint
+
+        # Make them as global classes and functions
+        global Stock, past, today, dprint
+
+
+init_yuki()
