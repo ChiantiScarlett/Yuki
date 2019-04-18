833934317fce6fb7c6ffc8d391c7bba9c2db8441
Chianti Scarlett <chianti.scarlett@gmail.com>
2019-04-18 19:09:10 +0900

    fixed minor error

--- core.py
+++ core.py
@@ -1,12 +1,7 @@
 from urllib.request import urlopen
 import json
 
-API = {
-    'summary': "",
-    'realtime': "{code}"
-}
-
 
 def _parse_data(url, isJSON=False):
-    html = urlopen(API[route].format(code=code)).read().decode('euc-kr')
+    html = urlopen(url).read().decode('euc-kr')
     return json.loads(html) if url else html
